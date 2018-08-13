746c12e3b81e8c7159bb556e990840d68d2b4f31
David Johnson <djohnson.m@gmail.com>
2018-08-12 23:08:53 -0400

    Semigroup fix
    Remove cpp on semigroup

--- src/GraphQL/AST.hs
+++ src/GraphQL/AST.hs
@@ -20,10 +20,7 @@ import Data.Data       (Data)
 import Data.Text       (Text)
 import Data.Hashable   (Hashable)
 import Control.DeepSeq (NFData)
-#if MIN_VERSION_base(4,10,0)
 import Data.Semigroup  (Semigroup)
-#else
-#endif
 --------------------------------------------------------------------------------
 import GraphQL.Lexer
 --------------------------------------------------------------------------------
